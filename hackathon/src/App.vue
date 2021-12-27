f3dfe4536d56b55dca68cd272d9623fb67286347
anirudhsnayak <anirudhsnayak@gmail.com>
2021-12-27 09:28:54 -0800

    Initial commit

--- hackathon/src/App.vue
+++ hackathon/src/App.vue
@@ -1,36 +1,15 @@
 <template>
   <div>
-    <h1>HALLOWEEN COSTUME RECOMMENDER</h1>
-    <InputBox />
   </div>
 </template>
 
 <script>
-import InputBox from "./components/InputBox";
-
 export default {
   name: "App",
   components: {
-    InputBox,
   },
 };
 </script>
+
 <style>
-@font-face {
-  font-family: "Halloween Morning";
-  src: local("Halloween Morning"), url(./fonts/Halloween-Morning.ttf);
-}
-html {
-  background-color: #16151d;
-}
-h1 {
-  color: #a21622;
-  background-color: #ff711f;
-  text-align: center;
-  padding: 20px;
-  font-size: 50px;
-  border-color: black;
-  font-family: "Halloween Morning";
-  margin-top: 0px;
-}
 </style>
